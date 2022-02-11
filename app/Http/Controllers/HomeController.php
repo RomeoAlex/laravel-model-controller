3b927fa98b2cbcab3a4fed127df2699d368b8709
RomeoAlex <romeo.alessandro17@gmail.com>
2022-02-11 17:46:03 +0100

    dati riportati ma scss non funziona!!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
+// chiamo il modello per richiamare il database movies
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class HomeController extends Controller
 {
     public function Index(){
-        return view('homepage');
+        // // attingo le informzioni passate dal model
+        $movies = Movie::all();
+        // stampo per verifica
+        
+        $data = [
+            'movies'=>$movies
+        ];
+
+        return view('homepage', $data);
     }
 }
+?>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,8 +1,17 @@
 <h1>SONO LA HOME PAGE</h1>
 
 <h1>Lista film</h1>
+@foreach ($movies as $movie)
+
 <h2>Titolo:</h2>
+<h2 class="beautify">{{ $movie['title'] }}</h2>
 <h3>Titolo originale</h3>
+<h3 class="beautify"> {{ $movie['original_title'] }}</h3>
 <h3>Nazionalità</h3>
+<h3 class="beautify">{{ $movie['nationality'] }}</h3>
 <h3>Data uscita</h3>
+<h3 class="beautify">{{ $movie['date'] }}</h3>
 <h3>Voto</h3>
+<h3 class="beautify">{{ $movie['vote'] }}</h3> 
+
+@endforeach
